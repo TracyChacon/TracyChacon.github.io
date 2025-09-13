4e84874830a3513ca640d6598c15cda936035fdd
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-09-13 18:43:36 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scrolling for navigation links
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault()
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                })
+            })
+        });
