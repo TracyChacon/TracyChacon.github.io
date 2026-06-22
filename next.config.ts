bbcb61e577a5359cf52b961fe2e86f5d99c08cec
tracy <tracy.chacon.00@gmail.com>
2026-06-21 19:09:07 -0500

    feat: add custom favicon logo to title

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   }
