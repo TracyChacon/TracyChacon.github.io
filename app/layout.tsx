66a5272f12c85a8a8b8b7c0ce7f38bffdbf2b75e
tracy <tracy.chacon.00@gmail.com>
2026-06-23 17:28:16 -0500

    feat: implement native GA4 script tag to root layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,25 @@ const jsonLd = {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8TMZ2KM2BY"
+        >
+        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8TMZ2KM2BY', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
